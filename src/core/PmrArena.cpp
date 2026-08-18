c49d68ee1772b1f34ca2ef78056fd4e3c86fbffa
moon3fire <arman.isajanyan18@gmail.com>
2026-08-18 05:29:41 +0400

    PmrArena.cpp incomplete fix

--- src/core/PmrArena.cpp
+++ src/core/PmrArena.cpp
@@ -4,7 +4,6 @@
 #include <sys/mman.h>
 #include <unistd.h>
 
-
 namespace cacheline {
 
 	explicit PmrArena::PmrArena(size_t capacityBytes) noexcept 
@@ -22,10 +21,43 @@ namespace cacheline {
 		m_rawBuffer = mmap(nullptr, m_capacityBytes, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB, -1, 0);
 
 		if (m_rawBuffer == MAP_FAILED) {
-
+			m_rawBuffer = mmap(nullptr, m_capacityBytes, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 		}
+
+		if (m_rawBuffer == MAP_FAILED) return false;
 // Platform dependent code end.
 
+		auto* ptr = static_cast<volatile uint8_t*>(m_rawBuffer);
+		for (size_t offset = 0; offset < m_capacityBytes; offset += 4096) {
+			ptr[offset] = 0;
+		}
+
+		m_bufferResource = std::make_unique<std::pmr::monotonic_buffer_resource>(m_rawBuffer, m_capacityBytes, std::pmr::null_memory_resource());
+
+		return true;
+	}
+
+	[[nodiscard]] std::pmr::memory_resource* PmrArena::GetResource() noexcept {
+		return m_bufferResource.get();
+	}
+
+	void PmrArena::Reset() noexcept {
+		if (m_bufferResource) {
+			m_bufferResource->release();
+			m_bufferResource = std::make_unique<std::pmr::monotonic_buffer_resource>(m_rawBuffer, m_capacityBytes, std::pmr::null_memory_resource());
+		}
+	}
+
+	void PmrArena::Release() noexcept {
+		if (!m_rawBuffer) 
+			return;
+
+		m_bufferResource.reset();
+
+// Platform dependent code start.
+		munmap(m_rawBuffer, m_capacityBytes);
+// Platform dependent code end.
+		m_rawBuffer = nullptr;
 	}
 
 } // namespace cacheline
